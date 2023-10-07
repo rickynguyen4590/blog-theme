fef4eed464e37eb41ed5cdd5ae023983b4c68cca
Ricky Nguyen <ricky.nguyen@noria.no>
2023-10-07 21:06:08 +0200

    (feat) Integrate with highlight

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,63 +1,65 @@
+import hljs from 'highlight.js';
+import CopyButtonPlugin from './copy';
+
 function idle(cb, time) {
-    time = time || 500 + Math.random() * 2500
-    setTimeout(cb, time);
+  time = time || 500 + Math.random() * 2500
+  setTimeout(cb, time);
 }
 
 function idleLong(cb, time) {
-    time = time || 1500 + Math.random() * 4000
-    setTimeout(cb, time);
+  time = time || 1500 + Math.random() * 4000
+  setTimeout(cb, time);
 }
 
 
 function loadFb() {
-    const script = document.createElement('script');
-    script.crossOrigin = "anonymous";
-    script.async = true;
-    script.nonce = "hRevMNbZ";
-    script.src = "https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v9.0&appId=429146018231335";
-    document.getElementsByTagName('head')[0].appendChild(script);
+  const script = document.createElement('script');
+  script.crossOrigin = "anonymous";
+  script.async = true;
+  script.nonce = "hRevMNbZ";
+  script.src = "https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v9.0&appId=429146018231335";
+  document.getElementsByTagName('head')[0].appendChild(script);
 }
 
 function loadGG() {
-    const script = document.createElement('script');
-    script.crossOrigin = "anonymous";
-    script.async = true;
-    script.src = "https://www.googletagmanager.com/gtag/js?id=G-XZG88T09VQ";
-    document.getElementsByTagName('head')[0].appendChild(script);
+  const script = document.createElement('script');
+  script.crossOrigin = "anonymous";
+  script.async = true;
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-XZG88T09VQ";
+  document.getElementsByTagName('head')[0].appendChild(script);
 
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    window.dataLayer = window.dataLayer || [];
+  window.dataLayer = window.dataLayer || [];
 
-    function gtag() {
-        dataLayer.push(arguments);
-    }
+  function gtag() {
+    dataLayer.push(arguments);
+  }
 
-    gtag('js', new Date());
-    gtag('config', 'G-XZG88T09VQ');
+  gtag('js', new Date());
+  gtag('config', 'G-XZG88T09VQ');
 }
 
 function highlight() {
-    document.querySelectorAll('pre code').forEach((block) => {
-        hljs.highlightBlock(block);
-        hljs.lineNumbersBlock(block);
-    });
+  hljs.addPlugin(new CopyButtonPlugin())
+  document.querySelectorAll('pre code').forEach((block) => {
+    hljs.highlightElement(block);
+  });
 }
 
 (function () {
-    pagination(true);
-    idleLong(loadFb);
-    idleLong(loadGG);
-    idle(highlight, 10);
+  pagination()
+  idleLong(loadFb);
+  idleLong(loadGG, 10);
+  idle(highlight, 10);
 
-    if (!document.body.classList.contains('post-template')) return;
+  if (!document.body.classList.contains('post-template')) return;
 
-    const cover = document.querySelector('.gh-cover');
-    if (!cover) return;
+  const cover = document.querySelector('.gh-cover');
+  if (!cover) return;
 
-    const image = cover.querySelector('.gh-cover-image');
+  const image = cover.querySelector('.gh-cover-image');
 
-    window.addEventListener('load', function () {
-        cover.style.setProperty('--cover-height', image.clientWidth * image.naturalHeight / image.naturalWidth + 'px');
-        cover.classList.remove('loading');
-    });
+  window.addEventListener('load', function () {
+    cover.style.setProperty('--cover-height', image.clientWidth * image.naturalHeight / image.naturalWidth + 'px');
+    cover.classList.remove('loading');
+  });
 })();
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const {series, parallel, watch, src, dest} = require('gulp');
 const pump = require('pump');
 
 // gulp plugins and utils
+const browserify = require('browserify');
 const livereload = require('gulp-livereload');
 const postcss = require('gulp-postcss');
 const concat = require('gulp-concat');
@@ -9,6 +10,10 @@ const uglify = require('gulp-uglify');
 const beeper = require('beeper');
 const zip = require('gulp-zip');
 const sassx = require('gulp-sass')(require('sass'));
+const babel = require('gulp-babel')
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const babelify = require("babelify");
 
 // postcss plugins
 const easyimport = require('postcss-easy-import');
@@ -16,68 +21,74 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 
 function serve(done) {
-    livereload.listen();
-    done();
+  livereload.listen();
+  done();
 }
 
 function handleError(done) {
-    return function (err) {
-        if (err) {
-            beeper();
-        }
-        return done(err);
-    };
-};
+  return function (err) {
+    if (err) {
+      beeper();
+    }
+    return done(err);
+  };
+}
 
 function hbs(done) {
-    pump([
-        src(['*.hbs', 'partials/**/*.hbs']),
-        livereload()
-    ], handleError(done));
+  pump([
+    src(['*.hbs', 'partials/**/*.hbs']),
+    livereload()
+  ], handleError(done));
 }
 
 function css(done) {
-    pump([
-        src('assets/css/screen.scss', {sourcemaps: true}),
-        sassx().on('error', sassx.logError),
-        postcss([
-            easyimport,
-            autoprefixer(),
-            cssnano()
-        ]),
-        dest('assets/built/', {sourcemaps: '.'}),
-        livereload()
-    ], handleError(done));
+  pump([
+    src('assets/css/screen.scss', {sourcemaps: true}),
+    sassx().on('error', sassx.logError),
+    postcss([
+      easyimport,
+      autoprefixer(),
+      cssnano()
+    ]),
+    dest('assets/built/', {sourcemaps: '.'}),
+    livereload()
+  ], handleError(done));
 }
 
 function js(done) {
-    pump([
-        src([
-            'node_modules/@tryghost/shared-theme-assets/assets/js/v1/lib/**/*.js',
-            'node_modules/@tryghost/shared-theme-assets/assets/js/v1/main.js',
-            'assets/js/lib/*.js',
-            'assets/js/main.js'
-        ], {sourcemaps: true}),
-        concat('main.min.js'),
-        uglify(),
-        dest('assets/built/', {sourcemaps: '.'}),
-        livereload()
-    ], handleError(done));
+  pump([
+    browserify({
+      entries: 'assets/js/main.js',
+      debug: true
+    }).transform(babelify.configure({
+      presets: ["@babel/preset-env"]
+    })).bundle(),
+    source('ghost.js'),
+    buffer(),
+    src([
+      'node_modules/@tryghost/shared-theme-assets/assets/js/v1/lib/**/*.js',
+      'node_modules/@tryghost/shared-theme-assets/assets/js/v1/main.js',
+    ], {sourcemaps: true}),
+    concat('main.min.js'),
+    uglify(),
+    dest('assets/built/', {sourcemaps: '.'}),
+    livereload()
+  ], handleError(done));
 }
 
 function zipper(done) {
-    const filename = require('./package.json').name + '.zip';
+  const filename = require('./package.json').name + '.zip';
 
-    pump([
-        src([
-            '**',
-            '!node_modules', '!node_modules/**',
-            '!dist', '!dist/**',
-            '!yarn-error.log'
-        ]),
-        zip(filename),
-        dest('dist/')
-    ], handleError(done));
+  pump([
+    src([
+      '**',
+      '!node_modules', '!node_modules/**',
+      '!dist', '!dist/**',
+      '!yarn-error.log'
+    ]),
+    zip(filename),
+    dest('dist/')
+  ], handleError(done));
 }
 
 const hbsWatcher = () => watch(['*.hbs', 'partials/**/*.hbs'], hbs);
